abf208ff812e3b246a3b20aaf11803aa7bd03461
Minh Chau <minh.chau@farfetch.com>
2022-09-20 11:26:35 +0100

    add dataset class

---
+++ covid_ode/covid_dataset.py
@@ -0,0 +1,15 @@
+from torch.utils.data import Dataset
+
+
+class CovidDataset(Dataset):
+    def __init__(self, data_frame, len_input, len_output):
+        self.data = data_frame
+        self.len_input = len_input
+        self.len_output = len_output
+
+    def __len__(self):
+        return self.data.shape[0] // (self.len_input + self.len_output)
+
+    def __getitem__(self, index):
+        return self.data[index*self.len_input : (index+1)*self.len_input], \
+               self.data[(index+1)*self.len_input : (index+1)*self.len_input+self.len_output]
